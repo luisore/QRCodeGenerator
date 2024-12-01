206973aeae8ccc516a3e11a9198e4e68eaa74803
luisore <luissk47@gmail.com>
2024-12-01 11:22:44 -0300

    Mi codigo QR

---
+++ qr.py
@@ -0,0 +1,16 @@
+import qrcode
+
+
+url = "https://youtube.com"
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=25,
+    border=5
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+imagen = qr.make_image()
+imagen.save("mi_qr.png")
